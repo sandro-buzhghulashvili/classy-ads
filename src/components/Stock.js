30628dc7029b60007ad07e0c63f8e9dc023c2959
Sandro <sbuzhghulashvili@gmail.com>
2023-11-22 23:19:44 +0400

    added hero page

---
+++ src/components/Stock.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import classes from './Stock.module.scss';
+import { Home, Book, Bed, Lightbulb, Car, Pizza } from 'lucide-react';
+
+const iconArray = [
+  <Home />,
+  <Book />,
+  <Bed />,
+  <Lightbulb />,
+  <Car />,
+  <Pizza />,
+];
+
+const Stock = () => {
+  const [ads, setAds] = useState([]);
+  const stock = {};
+
+  if (ads.length > 0) {
+    ads.forEach((ad, index) => {
+      if (!stock[ad.type]) {
+        stock[ad.type] = {
+          quantity: ad.productQuantity,
+          icon: iconArray[index],
+        };
+      } else {
+        stock[ad.type].quantity += ad.productQuantity;
+      }
+    });
+  }
+
+  console.log(Object.entries(stock));
+
+  const fetchAds = async () => {
+    const res = await fetch(
+      'https://classy-ads-8216b-default-rtdb.firebaseio.com/ads.json'
+    );
+    const data = await res.json();
+    setAds(Object.values(data)[0]);
+  };
+
+  useEffect(() => {
+    fetchAds();
+  }, []);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 0 }}
+      animate={{ opacity: 1, translateY: -200 }}
+      transition={{ duration: 1 }}
+      className={classes.stocks}
+    >
+      {Object.entries(stock).map((element) => (
+        <div className={classes.stock} key={element[0]}>
+          <div className={classes.icon}>{element[1].icon}</div>
+          <span>{element[0]}</span>
+          <p>{element[1].quantity}</p>
+        </div>
+      ))}
+    </motion.div>
+  );
+};
+
+export default Stock;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
-import classes from "./Landing.module.scss"
+import classes from './Landing.module.scss';
+import Stock from '../components/Stock';
 
 const LandingPage = () => {
-    return (
-        <main className={classes.hero}>
-            <h1>h</h1>
-        </main>
-    )
-}
+  const { scrollY } = useScroll();
+  const bgImageOpacity = useTransform(scrollY, [0, 500, 900], [1, 0.5, 0.1]);
+  const textY = useTransform(scrollY, [0, 400, 800], [0, -100, -200]);
+  const textScale = useTransform(scrollY, [0, 400, 800], [1, 0.8, 0.6]);
 
-export default LandingPage
+  return (
+    <>
+      <motion.main className={classes.hero} style={{ opacity: bgImageOpacity }}>
+        <motion.h1
+          initial={{ y: 400 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+          style={{ translateY: textY, scale: textScale }}
+        >
+          Largest Classifieds In The World
+        </motion.h1>
+        <motion.p
+          initial={{ y: 400 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+          style={{ translateY: textY, scale: textScale }}
+        >
+          You can buy, sell anything you want.
+        </motion.p>
+      </motion.main>
+      <Stock />
+    </>
+  );
+};
+
+export default LandingPage;
