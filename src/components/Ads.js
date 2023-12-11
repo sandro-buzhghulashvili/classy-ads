2bbf311b6bc116df1655013500bf276a0d799d2b
sandro <sbuzhghulashvili@gmail.com>
2023-12-11 10:34:42 +0400

    added filter

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import classes from './Ads.module.scss';
 import { Star } from 'lucide-react';
 
 export default function Ads({ products }) {
+  const [titleFilter,setTitleFilter] = useState('')
+  const [typeFilter, setTypeFilter] = useState('')
+
+  const [ads,setAds] = useState([])
+  const [index,setIndex] = useState(0)
+
+  let filterTypes = []
+
+  products.forEach(ad => {
+    if(!filterTypes.includes(ad.type)) {
+      filterTypes.push(ad.type)
+    }
+  })
+  
+
   const paginateArray = (arr, itemPerPage) => {
     let adArray = arr.slice();
     const pages = Math.ceil(arr.length / itemPerPage);
@@ -16,8 +31,6 @@ export default function Ads({ products }) {
 
     return transformedObject;
   };
-
-  console.log(paginateArray(products, 3));
 
   const displayStarRatings = (rating) => {
     const fullStars = Array.from({ length: rating }, (_, index) => (
@@ -36,12 +49,51 @@ export default function Ads({ products }) {
     );
   };
 
+  const paginationHandler = (newIndex) => {
+    setIndex(newIndex)
+  }
+
+  const titleFilterHandler = event => {
+    setTitleFilter(event.target.value)
+
+    const filteredProducts = products.filter(product => product.title.toLowerCase().includes(event.target.value.toLowerCase()))
+
+    setAds(paginateArray(filteredProducts, 4))
+  }
+
+  const typeFilterHandler = event => {
+    let filteredProducts = []
+
+    if(titleFilter.length === 0) {
+      if(event.target.value === 'default') {
+        filteredProducts = products
+      } else {
+        filteredProducts = products.filter(product => product.type === event.target.value)
+      }
+    } else {
+      if(event.target.value === 'default') {
+        filteredProducts = products.filter(product => product.title.toLowerCase().includes(titleFilter.toLowerCase()))
+      } else {
+        filteredProducts = products.filter(product => product.type === event.target.value && product.title.toLowerCase().includes(titleFilter.toLowerCase()))
+      }
+    }
+    setAds(paginateArray(filteredProducts, 4))
+  }
+
+  useEffect(() => {
+    if(ads.length === 0)  setAds(paginateArray(products, 4))
+  }, [products, ads])
+
+  // [].concat(...Object.values(ads)) -> Array form of ads object
+
+  console.log(ads)
+
   return (
     <div className={classes.ads}>
-      <div className={classes.products}>
-        {products.map((product) => {
-          {
-            return (
+      {
+        Object.values(ads).length > 0 &&
+        <div className={classes.products}>
+          {ads[index].map(product =>     
               <div className={classes.product} key={products.indexOf(product)}>
                 <img src={product.img} alt="ad" />
                 <span className={classes.type}>{product.type}</span>
@@ -52,12 +104,30 @@ export default function Ads({ products }) {
                   <p>( {product.reviewQuantity} reviews )</p>
                 </div>
               </div>
-            );
-          }
-        })}
-      </div>
+          )}
+
+          <ul className={classes.pagination}>
+            {Object.values(ads).map((_,adIndex) => {
+              return (
+                <li key={adIndex} className={index === adIndex ? classes.active : undefined } onClick={() => {paginationHandler(adIndex)}}>{adIndex + 1}</li>
+              )
+            })}
+          </ul>
+        </div>
+      }
       <div className={classes.filter}>
-        <h1>Filter</h1>
+        <h2>Filters</h2>
+        <input type="text" placeholder='Title' value={titleFilter} onChange={titleFilterHandler} />
+        <select onChange={typeFilterHandler}>
+          <option value={'default'}>All Categories</option>
+          {
+            filterTypes.map(type => {
+              return (
+                <option key={type}>{type}</option>
+              )
+            })
+          }
+        </select>
       </div>
     </div>
   );
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const LayoutPage = () => {
-  const navigation = useNavigation();
   return (
     <>
       {/* {navigation.state === 'loading' && <Navbar />} */}
